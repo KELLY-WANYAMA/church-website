04356ce2741b327c461e36e91b75c0511bea6bd1
KELLY-WANYAMA <wanyamakelvin47@gmail.com>
2025-08-23 11:56:04 +0300

    changes

--- nairobi/settings.py
+++ nairobi/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 SECRET_KEY = 'django-insecure--2bo7m+d=#q+=-c7cbs5_8w1u8wzd-0s6ftn^h@sb8-@pkwi4#'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
